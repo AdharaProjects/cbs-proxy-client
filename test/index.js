7be88cafdb0fa73fe5148a4e0b6c66fea55f33ac
Jason Smythe <jason@smythemail.za.net>
2018-08-02 12:26:08 +0300

    [#7] Working basic implementation.

--- test/index.js
+++ test/index.js
@@ -18,7 +18,7 @@ describe("The core banking system proxy", function() {
   let user1ProxyClient
   before (async () => {
     adminProxyClient = await cbsProxyClient(config.cbsUnameAdmin, config.cbsPasswordAdmin, config.cbsProxyUrl)
-    user1ProxyClient = await cbsProxyClient(config.cbsUnameAdmin, config.cbsPasswordAdmin, config.cbsProxyUrl)
+    user1ProxyClient = await cbsProxyClient(config.cbsUnameUser1, config.cbsPasswordUser1, config.cbsProxyUrl)
   })
 
 
@@ -28,12 +28,11 @@ describe("The core banking system proxy", function() {
     before (async () => {
       const sessionToken = await getSessionToken(config.cbsUnameUser1, config.cbsPasswordUser1, config.cbsProxyUrl)
       transfers = await makeRandomTransfersToOmnibusAccount(11, sessionToken)
-      sleep(5000) // You need to sleep for a bit here because cyclos is slow...
     })
-    it("returns a list of (credit) transfers to the omnibus account after a certain date", async () => {
+    it("returns a list of TO transfers to the omnibus account after a certain date", async () => {
       for (let i = 0; i < transfers.length; ++i) {
         const omnibusCreditsFromTimestamp = await adminProxyClient.getTransfersToOmnibusAccount(transfers[i].timestamp)
-        console.log(omnibusCreditsFromTimestamp.transfers.length)
+        console.log(omnibusCreditsFromTimestamp.transfers.length, 'transfers occured since', Date(transfers[i].timestamp * 1000))
         expect(omnibusCreditsFromTimestamp.transfers.length).to.be.equal(transfers.length - i)
       }
     })
@@ -45,14 +44,40 @@ describe("The core banking system proxy", function() {
     before (async () => {
       const sessionToken = await getSessionToken(config.cbsUnameAdmin, config.cbsPasswordAdmin, config.cbsProxyUrl)
       transfers = await makeRandomTransfersFromOmnibusAccount(11, sessionToken)
-      sleep(5000) // You need to sleep for a bit here because cyclos is slow...
     })
-    it("returns a list of (credit) transfers to the omnibus account after a certain date", async () => {
+    it("returns a list of FROM transfers to the omnibus account after a certain date", async () => {
       for (let i = 0; i < transfers.length; ++i) {
         const omnibusCreditsFromTimestamp = await adminProxyClient.getTransfersFromOmnibusAccount(transfers[i].timestamp)
-        console.log(omnibusCreditsFromTimestamp.transfers.length)
+        console.log(omnibusCreditsFromTimestamp.transfers.length, 'transfers occured since', Date(transfers[i].timestamp * 1000))
         expect(omnibusCreditsFromTimestamp.transfers.length).to.be.equal(transfers.length - i)
       }
+    })
+  })
+
+  describe('makeTransferToOmnibusAccount', () => {
+    let toOmnibusTimestamp
+
+    before (async () => {
+      toOmnibusTimestamp = new Date() / 1000
+      user1ProxyClient.makeTransferToOmnibusAccount(5, 'test-to omnibus account')
+      await sleep(100) // You need to sleep for a bit here because cyclos is slow...
+    })
+    it("returns a list of (credit) transfers to the omnibus account after a certain date", async () => {
+      const omnibusCreditsFromTimestamp = await adminProxyClient.getTransfersToOmnibusAccount(toOmnibusTimestamp)
+      expect(omnibusCreditsFromTimestamp.transfers.length).to.be.equal(1)
+    })
+  })
+  describe('makeTransferFromOmnibusAccount', () => {
+    let fromOmnibusTimestamp
+
+    before (async () => {
+      fromOmnibusTimestamp = new Date() / 1000
+      adminProxyClient.makeTransferFromOmnibusAccount(5, 'test-from omnibus account', config.cbsAccountUser1)
+      await sleep(1000) // You need to sleep for a bit here because cyclos is slow...
+    })
+    it("returns a list of (credit) transfers to the omnibus account after a certain date", async () => {
+      const omnibusCreditsFromTimestamp = await adminProxyClient.getTransfersFromOmnibusAccount(fromOmnibusTimestamp)
+      expect(omnibusCreditsFromTimestamp.transfers.length).to.be.equal(1)
     })
   })
 })
