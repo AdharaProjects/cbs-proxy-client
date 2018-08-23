688a417e5a394680483885840a038b477b01caca
Jason Smythe <jason@smythemail.za.net>
2018-08-23 15:16:51 +0300

    #12 add working tests

--- test/index.js
+++ test/index.js
@@ -29,17 +29,25 @@ describe("The core banking system proxy", function() {
       const sessionToken = await getSessionToken(config.cbsUnameUser1, config.cbsPasswordUser1, config.cbsProxyUrl)
       transfers = await makeRandomTransfersToOmnibusAccount(11, sessionToken)
     })
-    it("returns a list of TO transfers to the omnibus account after a certain date", async () => {
+    it("returns a list of TO transfers of the omnibus account after a certain date", async () => {
       for (let i = 0; i < transfers.length; ++i) {
         const omnibusCreditsFromTimestamp = await adminProxyClient.getTransfersToOmnibusAccount(transfers[i].timestamp)
         console.log(omnibusCreditsFromTimestamp.transfers.length, 'transfers occured since', Date(transfers[i].timestamp * 1000))
         expect(omnibusCreditsFromTimestamp.transfers.length).to.be.equal(transfers.length - i)
       }
     })
+    it("returns a list of FROM transfers of user account after a certain date", async () => {
+      for (let i = 0; i < transfers.length; ++i) {
+        const userDebitTrnsferFromTimestamp = await adminProxyClient.getTransfersToOmnibusAccount(transfers[i].timestamp)
+        console.log(userDebitTrnsferFromTimestamp.transfers.length, 'transfers occured since', Date(transfers[i].timestamp * 1000))
+        expect(userDebitTrnsferFromTimestamp.transfers.length).to.be.equal(transfers.length - i)
+      }
+    })
   })
 
   describe('getTransfersFromOmnibusAccount', () => {
     let transfers
+    let user
 
     before (async () => {
       const sessionToken = await getSessionToken(config.cbsUnameAdmin, config.cbsPasswordAdmin, config.cbsProxyUrl)
@@ -52,15 +60,23 @@ describe("The core banking system proxy", function() {
         expect(omnibusCreditsFromTimestamp.transfers.length).to.be.equal(transfers.length - i)
       }
     })
+
   })
 
   describe('makeTransferToOmnibusAccount', () => {
     let toOmnibusTimestamp, tx
     const transferAmount = '5.43'
     const message = 'test-to omnibus account'
+    let omnibusSummaryBefore
+    let omnibusSummaryAfter
+    let userSummaryBefore
+    let userSummaryAfter
 
     before (async () => {
       toOmnibusTimestamp = new Date() / 1000
+      omnibusSummaryBefore = await adminProxyClient.getAccountDetails()
+      userSummaryBefore = await user1ProxyClient.getAccountDetails()
+
       await sleep(500) // You need to sleep for a bit here because cyclos is slow...
       tx = await user1ProxyClient.makeTransferToOmnibusAccount(transferAmount, message)
       await sleep(1000) // You need to sleep for a bit here because cyclos is slow...
@@ -72,14 +88,40 @@ describe("The core banking system proxy", function() {
       expect(omnibusToTransfersFromTimestamp.transfers[0].amount).to.be.equal(transferAmount)
       expect(omnibusToTransfersFromTimestamp.transfers[0].description).to.be.equal(message)
     })
+    it("should reflect the correct amonut in the omnibus Account summary after transfers", async () => {
+      omnibusSummaryAfter = await adminProxyClient.getAccountDetails()
+
+      expect(
+        omnibusSummaryAfter.status.balance
+      ).to.be.equal(
+        // NOTE:: Beware of the rounding error!
+        (parseFloat(omnibusSummaryBefore.status.balance) + parseFloat(transferAmount)).toFixed(2)
+      )
+    })
+    it("should reflect the correct amonut in the user account summary after transfers", async () => {
+      userAccountSummaryAfter = await user1ProxyClient.getAccountDetails()
+
+      expect(
+        userAccountSummaryAfter.status.balance
+      ).to.be.equal(
+        // NOTE:: Beware of the rounding error!
+        (parseFloat(userSummaryBefore.status.balance) - parseFloat(transferAmount)).toFixed(2)
+      )
+    })
   })
   describe('makeTransferFromOmnibusAccount', () => {
     let fromOmnibusTimestamp, tx
     const transferAmount = '3.11'
     const message = 'test-from omnibus account'
+    let omnibusSummaryBefore
+    let omnibusSummaryAfter
+    let userSummaryBefore
+    let userSummaryAfter
 
     before (async () => {
       fromOmnibusTimestamp = new Date() / 1000
+      omnibusSummaryBefore = await adminProxyClient.getAccountDetails()
+      userSummaryBefore = await user1ProxyClient.getAccountDetails()
       tx = await adminProxyClient.makeTransferFromOmnibusAccount(transferAmount, message, config.cbsAccountUser1)
       await sleep(1000) // You need to sleep for a bit here because cyclos is slow...
     })
@@ -89,6 +131,26 @@ describe("The core banking system proxy", function() {
       expect(omnibusFromTransfersFromTimestamp.transfers[0].id).to.be.equal(tx.transferId)
       expect(omnibusFromTransfersFromTimestamp.transfers[0].amount).to.be.equal('-' + transferAmount)
       expect(omnibusFromTransfersFromTimestamp.transfers[0].description).to.be.equal(message)
+    })
+    it("should reflect the correct amonut in the omnibus Account summary after transfers", async () => {
+      omnibusSummaryAfter = await adminProxyClient.getAccountDetails()
+
+      expect(
+        omnibusSummaryAfter.status.balance
+      ).to.be.equal(
+        // NOTE:: Beware of the rounding error!
+        (parseFloat(omnibusSummaryBefore.status.balance) - parseFloat(transferAmount)).toFixed(2)
+      )
+    })
+    it("should reflect the correct amonut in the user account summary after transfers", async () => {
+      userAccountSummaryAfter = await user1ProxyClient.getAccountDetails()
+
+      expect(
+        userAccountSummaryAfter.status.balance
+      ).to.be.equal(
+        // NOTE:: Beware of the rounding error!
+        (parseFloat(userSummaryBefore.status.balance) + parseFloat(transferAmount)).toFixed(2)
+      )
     })
   })
 })
